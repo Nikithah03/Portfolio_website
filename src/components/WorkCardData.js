024d440845666285c076ce774e3fa91b06e55f65
Nikithah03 <nikitha.harish03@gmail.com>
2024-09-17 17:59:23 +0530

    initial commit

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assests/project1.png";
+import pro2 from "../assests/project2.png";
+import pro3 from "../assests/project3.png";
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Ecommerce Website Design",
+        text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded,tangible and self-affirming sense of .Wow! I really can do this for over 100,000 online enterpreneurs. Best-selling marketing courses",
+        view:"https://www.youtube.com/watch?v=99muDSuP55s&list=PL9bD98LkBR7P8MYh0RzNSHgeVNTA8g0nB&index=2&pp=iAQB"
+    },
+    {
+        imgsrc: pro2,
+        title: "LMS/Course Website Design",
+        text: "Podacast Thriving Facebook community and top-ranked. Amy inspires a grounded, tangible and self-affirming sense of Wow! I really can do this for over 100,000 online enterpreneurs. Best-selling marketing courses",
+        view:"https://www.youtube.com/watch?v=99muDSuP55s&list=PL9bD98LkBR7P8MYh0RzNSHgeVNTA8g0nB&index=2&pp=iAQB"
+    },
+    {
+        imgsrc: pro3,
+        title: "Rymo Ecommerce Design",
+        text: "Top ranked Thriving Facebook community and podcast  . Amy inspires a grounded,tangible and self-affirming sense of Wow! I raelly can do this for over 100,000 online enterpreneurs. Best-selling marketing courses",
+        view:"https://www.youtube.com/watch?v=99muDSuP55s&list=PL9bD98LkBR7P8MYh0RzNSHgeVNTA8g0nB&index=2&pp=iAQB"
+    },
+];
+
+export default ProjectCardData;
